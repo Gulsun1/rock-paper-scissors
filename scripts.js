dc92c951f12d0c2e87a8da47ace524f44fdd3a28
Alex <166944852+Gulsun1@users.noreply.github.com>
2024-07-03 22:15:04 +0200

    Define function for user choice and validation

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {          // Defines function that returns "Rock", "Paper" or "Scissors" randomly
-    
+
     let randomValue = Math.random();    // Assigns a random value to a variable
     
     if (randomValue < 1/3) {            // If the random value is less than 1/3 returns "Rock"
@@ -10,3 +10,15 @@ function getComputerChoice() {          // Defines function that returns "Rock",
         return "Scissors";              // If the random value is of any other value returns "Scissors"
     }
 }
+
+function getHumanChoice() {     // Defines functions that validates and returns the user's choice
+
+    let humanChoice = prompt("Rock, Paper or Scissors?", "").toLowerCase();     // Assigns the user's choice to a variable and makes it case insensitive
+
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {       // Loops prompt as long as the choice is not valid
+        console.log("Incorrect input, please select between Rock, Paper or Scissors");
+        humanChoice = prompt("Incorrect input, please select between Rock, Paper or Scissors", "").toLowerCase();
+    }
+        return humanChoice;     // Returns the user's choice when valid
+}
+
